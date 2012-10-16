2a3d46621f018d8724d3c3146de7eb4cd46585d8
regerege <regerege6@gmail.com>
2012-10-16 22:01:25 +0900

    first commit

---
+++ FsDirectShow/AssemblyInfo.fs
@@ -0,0 +1,26 @@
+﻿namespace TVTestChannelEditor
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// アセンブリの署名
+[<assembly: AssemblyKeyFile("FsDirectShow.snk")>]
+
+// アセンブリ情報
+[<assembly: AssemblyTitle("FsDirectShow")>]
+[<assembly: AssemblyDescription("DirectShow for FSharp")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("FsDirectShow")>]
+[<assembly: AssemblyCopyright("Copyright© 2012 regerege  All Rights Reserved.")>]
+[<assembly: AssemblyTrademark("")>]
+//[<assembly: AssemblyCulture("")>]
+
+[<assembly: ComVisible(false)>] 
+
+[<assembly: Guid("4E8918C7-B4E4-4900-8B45-54C19D26E206")>]
+
+[<assembly: AssemblyVersion("0.0.*.*")>]
+[<assembly: AssemblyFileVersion("0.0.*.*")>]
+()
---
+++ README.md
@@ -0,0 +1,33 @@
+# Table of Contents (目次)
+
+ちょっとテスト
+<ul>
+<li>[Licens](#Licens)</li>
+<li>[Readme(日本語)](#Readme(日本語))
+  <ul>
+    <li>[FsDirectShowについて](#FsDirectShowについて)</li>
+  </ul></li>
+<li>[Readme(English)](#Readme(English))
+  <ul>
+    <li>[About FsDirectShow](#FsDirectShowについて)</li>
+  </ul></li>
+</ul>
+
+#Licens
+
+Licensed under Lesser General Public License.  See license.txt for details.
+
+# 日本語
+
+## FsDirectShowについて
+
+マーシャリングの練習＆DirectShowの使い方を理解するために、[DirectShow.NET Library](http://directshownet.sourceforge.net/)を移植するために作成したプロジェクトです。
+
+
+# Readme(English)
+
+## About FsDirectShow
+
+In order to understand how to use the practice of marshaling & DirectShow is a project [DirectShow.NET Library](http://directshownet.sourceforge.net/) was created to port.
+
+
