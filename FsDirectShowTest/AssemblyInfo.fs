76d6924a08d60c64c39f92e80d5f765f8e10cb30
regerege <regerege6@gmail.com>
2012-10-21 08:13:14 +0900

    Add TestProject using MbUnit

---
+++ FsDirectShowTest/AssemblyInfo.fs
@@ -0,0 +1,27 @@
+﻿namespace DirectShowLibTest
+
+open System
+open System.Reflection
+open System.Runtime.InteropServices
+
+// アセンブリの署名
+[<assembly: AssemblyKeyFile("FsDirectShowTest.snk")>]
+
+// アセンブリ情報
+[<assembly: AssemblyTitle("FsDirectShowTest")>]
+[<assembly: AssemblyDescription("DirectShowTest for FSharp")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("FsDirectShowTest")>]
+[<assembly: AssemblyCopyright("Copyright© 2012 regerege All Rights Reserved.")>]
+[<assembly: AssemblyTrademark("")>]
+//[<assembly: AssemblyCulture("")>]
+
+[<assembly: ComVisible(false)>]
+[<assembly: CLSCompliant(true)>]
+
+[<assembly: Guid("D59F4433-2353-45E7-B9D8-12E883AC0EA3")>]
+
+[<assembly: AssemblyVersion("0.0.1.*")>]
+[<assembly: AssemblyFileVersion("0.0.1.*")>]
+()
---
+++ FsDirectShowTest/ICreateDevEnumTest.fs
@@ -0,0 +1,15 @@
+﻿namespace FsDirectShowTest
+open MbUnit.Framework
+open MbUnit.Framework.ContractVerifiers
+open System
+open FsDirectShow
+open FsDirectShow.CoClassesHelper
+
+[<TestFixtureAttribute>]
+type ICreateDevEnumTests() =
+    [<Test>]
+    member x.CreateCreateDevEnumTests() =
+        let ic = CreateDevEnum()
+        Assert.IsNotNull(ic)
+        GC.Collect()
+
